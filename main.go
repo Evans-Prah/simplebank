f10c30a385d39559272d9ea09649c12e6a277383
Evans Prah <evans.prah@hubtel.com>
2023-11-27 19:37:08 +0000

    load config from file and environment variables

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/Evans-Prah/simplebank/api"
 	db "github.com/Evans-Prah/simplebank/db/sqlc"
+	"github.com/Evans-Prah/simplebank/db/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to the db", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 			log.Fatal("cannot connect to the server", err)
 	}
